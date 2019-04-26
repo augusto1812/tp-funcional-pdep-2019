18b4d8b986728bd1eb6d29edb67bc70f6ae7f00b
Leticia Siani <37660898+letisiani@users.noreply.github.com>
2019-04-26 15:41:02 -0300

    Update entrega-1.hs

--- entrega-1.hs
+++ entrega-1.hs
@@ -6,31 +6,34 @@ data Participante = Participante {
                                   nombre :: String, 
                                   nivelDeNafta :: Int, 
                                   velocidad :: Int, 
-                                  enamorade :: Participante
-                                  -- truco :: } 
+                                  enamorade :: Participante}
+                                  -- truco :: }
                                   deriving (Show)
+                                  
+--Autos
 
---PUNTO 1: 2) 
- deReversa :: Participante -> Participante
- deReversa unParticipante = unParticipante  {nivelDeNafta = ((+nivelDeNafta).(/5)) unParticipante}
- --preguntar por qué te dice que la pista tiene 100m, o sea que siempre la distancia va a ser 200 (1/5 de mil) 
- --sino seria esta la funcion: -- deReversa = (+200)
-
- impresionar :: Participante -> Participante
- impresionar unParticipante = unParticipante {velocidad = (*2)}
-
- nitro :: Participante-> Participante
- nitro unParticipante = unParticipante {velocidad = (+15)}
-
---no se como hacer fingir amor
-
---PUNTO 1: 3) 
- rochaMcQueen = Participante { nombre = "Rocha McQueen", nivelDeNafta = 300, velocidad = 0 -- enamorade = "ronco" -- truco = deReversa }
+rochaMcQueen = Participante { nombre = "Rocha McQueen", nivelDeNafta = 300, velocidad = 0 -- enamorade = "ronco" -- truco = deReversa }
 
 --No entiendo si su enamorade va a ser otro participante (de tipo data) o solo ponemos el nombre --de su enamorade ( o sea, un String)
 
- biankerr = Participante { nombre = "Biankerr", nivelDeNafta = 500, velocidad = 20 -- enamorade = tinch --truco = impresionar }
+biankerr = Participante { nombre = "Biankerr", nivelDeNafta = 500, velocidad = 20 -- enamorade = tinch --truco = impresionar }
 
- gushtav = Participante { nombre = "Gushtav", nivelDeNafta = 200, velocidad = 130 -- enamorada = PetiLaLinda -- truco = nitr }
+gushtav = Participante { nombre = "Gushtav", nivelDeNafta = 200, velocidad = 130 -- enamorada = PetiLaLinda -- truco = nitr }
 
- rodra = Participante { nombre = "Rodra", nivelDeNafta = 0, velocidad = 50 -- enamorada = Taisa -- truco = fingirAmor con petra ??? no se como hacer esto ayuda }
+rodra = Participante { nombre = "Rodra", nivelDeNafta = 0, velocidad = 50 -- enamorada = Taisa -- truco = fingirAmor con petra ??? no se como hacer esto ayuda }
+
+--PUNTO 1: 2) 
+deReversa :: Participante -> Participante
+deReversa unParticipante = unParticipante  {nivelDeNafta = ((+200).nivelDeNafta) unParticipante}
+ --preguntar por qué te dice que la pista tiene 100m, o sea que siempre la distancia va a ser 200 (1/5 de mil) 
+ --sino seria esta la funcion: -- deReversa = (+200)
+nitro :: Participante-> Participante
+impresionar unParticipante = unParticipante {velocidad = ((*2).velocidad) unParticipante}
+
+nitro :: Participante-> Participante
+nitro unParticipante = unParticipante {velocidad = ((+15).velocidad) unParticipante}
+
+--no se como hacer fingir amor
+
+
+ 
