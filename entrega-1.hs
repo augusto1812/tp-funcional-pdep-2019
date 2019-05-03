4de0a2d3ba0db6f2301db12736e3e718706cadfc
Julieta Romero <julieromero@est.frba.utn.edu.ar>
2019-05-02 22:17:53 -0300

    Agregado de punto 4 completo.

--- entrega-1.hs
+++ entrega-1.hs
@@ -11,32 +11,59 @@ data Participante = Participante {
                                   deriving (Show)
                                   
 -- Autos
-rochaMcQueen  = Participante { nombre = "Rocha McQueen", nivelDeNafta = 300, velocidad = 0, enamorade = "Ronco", truco = "deReversa" }
+rochaMcQueen  = Participante { 
+    nombre = "Rocha McQueen",
+    nivelDeNafta = 300,
+    velocidad = 0,
+    enamorade = "Ronco"
+ -- truco = deReversa 
+}
 
-biankerr      = Participante { nombre = "Biankerr", nivelDeNafta = 500, velocidad = 20,  enamorade = "Tinch", truco = "impresionar" }
+biankerr = Participante { 
+    nombre = "Biankerr", 
+    nivelDeNafta = 500, 
+    velocidad = 20,  
+    enamorade = "Tinch"
+    --truco = impresionar 
+}
 
-gushtav       = Participante { nombre = "Gushtav", nivelDeNafta = 200, velocidad = 130, enamorade = "PetiLaLinda", truco = "nitro"}
+gushtav = Participante {
+    nombre = "Gushtav", 
+    nivelDeNafta = 200, 
+    velocidad = 130, 
+    enamorade = "PetiLaLinda"
+    -- truco = nitro 
+}
 
-rodra         = Participante { nombre = "Rodra", nivelDeNafta = 0, velocidad = 50, enamorade = "Taisa", truco = "fingirAmor" }
+rodra = Participante { 
+    nombre = "Rodra", 
+    nivelDeNafta = 0, 
+    velocidad = 50, 
+    enamorade = "Taisa"
+    -- truco = fingirAmor petra 
+}
 
 -- PUNTO 1.2
 -- Trucos
 deReversa :: Participante -> Participante
-deReversa uneParticipante = uneParticipante  {nivelDeNafta = ((+200).nivelDeNafta) uneParticipante}
+deReversa uneParticipante = 
+    uneParticipante  {nivelDeNafta = ((+200).nivelDeNafta) uneParticipante}
 -- Con la pista de 1000m, la función aumenta siempre 200 (1000/5) y sería
 -- deReversa = (+200)
 
 impresionar :: Participante -> Participante
-impresionar uneParticipante = uneParticipante {velocidad = ((*2).velocidad) uneParticipante}
+impresionar uneParticipante = 
+    uneParticipante {velocidad = ((*2).velocidad) uneParticipante}
 
 nitro :: Participante -> Participante
-nitro uneParticipante = uneParticipante {velocidad = ((+15).velocidad) uneParticipante}
+nitro uneParticipante = 
+    uneParticipante {velocidad = ((+15).velocidad) uneParticipante}
 
 fingirAmor :: Participante -> String -> Participante 
-fingirAmor uneParticipante nueveEnamorade = uneParticipante {enamorade = nueveEnamorade}
+fingirAmor uneParticipante nueveEnamorade = 
+    uneParticipante {enamorade = nueveEnamorade}
 
 -- PUNTO 2
-incrementarVelocidad :: Participante -> Participante
 vocales :: String -> Bool
 vocales  = any ( 'a' || 'e' || 'i' || 'o' || 'u' ) 
 
@@ -52,21 +79,24 @@ incrementarVelocidad (Participante _ _ velocidad enamorade _ ) =
 incrementarVelocidad (Participante _ _ velocidad enamorade _ ) = 
     (largoDelNombreDeLaEnamorada == 3) (||) . (largoDelNombreDeLaEnamorada == 4) = velocidad + 20
 incrementarVelocidad (Participante _ _ velocidad enamorade _ ) = 
-    (largoDelNombreDeLaEnamorada > 4) = velocidad + 30- PUNTO 3
-
+    (largoDelNombreDeLaEnamorada > 4) = velocidad + 30
+    
+-- PUNTO 3
 puedeRealizarTruco :: Participante -> Bool
-puedeRealizarTruco (Participante _ nafta velocidad _ _) = (nafa != 0) && (velocidad < 100) 
-
-cambiarEnamorada :: Participante -> Participante
-cambiarEnamorada (Participante _ _ _ enamorade _ ) = fingirAmor
-
-queTrucazo :: Participante -> Participante
-queTrucazo (Participante _ _ velocidad enamorade _) = (incrementarVelocidad) . (cambiarEnamorada)
+puedeRealizarTruco (Participante _ nivelDeNafta velocidad _) = 
+    (nivelDeNafta /= 0) && (velocidad < 100)
 
 -- PUNTO 4
 -- Nuevos trucos
 comboLoco :: Participante -> Participante
+comboLoco = deReversa.nitro 
 
 queTrucazo :: Participante -> Participante
+queTrucazo uneParticipante = 
+  incrementarVelocidad.fingirAmor
 
 turbo :: Participante -> Participante
+turbo uneParticipante =
+    uneParticipante {velocidad = ((*10).nivelDeNafta) uneParticipante, nivelDeNafta = 0}
+
+
