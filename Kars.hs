35b8d7a0bdef3c281c0feef98de46e42425784cb
letisiani <lsianirambado@est.frba.utn.edu.ar>
2019-05-19 12:39:26 -0300

    Actualizado

--- Kars.hs
+++ Kars.hs
@@ -1,6 +1,6 @@
 import Text.Show.Functions
 
-module Kars where
+--module Kars where
 
 data Participante = 
     Participante { 
@@ -16,8 +16,8 @@ data Carreras =
         cantidadVueltas :: Int,
         longitudDePista :: Int,
         publico :: [String],
-        trampa :: (Carreras-> Carreras),
-        participantes :: [Participante]
+        participantes :: [Participante],
+        trampa :: (Carreras-> [Participante])
     } deriving (Show)
 
 ---------------------------------------------------
@@ -32,11 +32,51 @@ rochaMcQueen  = Participante {
     truco = deReversa 
 }
 
+biankerr = Participante { 
+    nombre = "Biankerr", 
+    nivelDeNafta = 500, 
+    velocidad = 20,  
+    enamorade = "Tinch",
+    truco = impresionar 
+}
+
+gushtav = Participante {
+    nombre = "Gushtav", 
+    nivelDeNafta = 200, 
+    velocidad = 130, 
+    enamorade = "PetiLaLinda",
+    truco = nitro 
+}
+
+rodra = Participante { 
+    nombre = "Rodra", 
+    nivelDeNafta = 0, 
+    velocidad = 50, 
+    enamorade = "Taisa",
+    truco = fingirAmor ("PetiLaLinda")
+}
+
+
+impresionar :: Participante -> Participante
+impresionar = modificarVelocidad (*2)
+
+nitro :: Participante -> Participante
+nitro = modificarVelocidad (+15)
+
+fingirAmor :: String -> Participante -> Participante 
+fingirAmor nueveEnamorade uneParticipante = 
+    uneParticipante {enamorade = nueveEnamorade}
+
+modificarVelocidad :: (Int -> Int) -> Participante -> Participante
+modificarVelocidad func (Participante nombre nivelDeNafta velocidad enamorade truco) =
+    Participante nombre nivelDeNafta (func velocidad) enamorade truco
+
+
 ---------------------------------------------------
 --------------------- CARRERA ---------------------
 ---------------------------------------------------
 
-potreroFunes = Carrera {
+potreroFunes = Carreras {
     cantidadVueltas = 3,
     longitudDePista = 5,
     publico = ["Ronco", "Tinch", "Dodain"],
@@ -52,10 +92,14 @@ deReversa :: Participante -> Participante
 deReversa (Participante nombre nivelDeNafta velocidad enamorade truco ) = 
  	Participante nombre (nivelDeNafta + div velocidad 5) velocidad enamorade truco
 
----------------------------------------------------
+------------ddads---------------------------------------
 ----------------------- 3.2 -----------------------
 ---------------------------------------------------
-
-podio :: Carrera -> Carrera
-podio (Carrera cantidadVueltas longitudDePista publico trampa participantes) =
-    Carrera cantidadVueltas longitudDePista publico trampa (take 3 participantes)
+sacarAlPistero :: Carreras -> [Participante]
+sacarAlPistero (Carreras _ _ _  participantes _ ) =
+	  tail participantes
+podio :: Carreras -> [Participante]
+podio (Carreras _ _ _  participantes _) = 
+     take 3 participantes
+lluvia :: Participante -> Participante
+lluvia = modificarVelocidad --nose|
